cd3852e4aeace5994155e11ebc56d036bb7b6263
JanTheThird <20122613@s.ubaguio.edu>
2026-03-09 21:48:18 +0800

    Created students page

---
+++ student-app/src/Pages/Students.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function Students() {
+
+  const [students, setStudents] = useState([]);
+
+  const studentData = [
+    { id:1, name:"Juan Cruz", course:"BSIT" },
+    { id:2, name:"Maria Santos", course:"BSCS" },
+    { id:3, name:"Pedro Reyes", course:"BSIT" },
+    { id:4, name:"Ana Lopez", course:"BSIS" },
+    { id:5, name:"Carlo Ramos", course:"BSCS" },
+    { id:6, name:"Liza Gomez", course:"BSIT" },
+    { id:7, name:"Mark Tan", course:"BSIS" },
+    { id:8, name:"Nina Cruz", course:"BSCS" },
+    { id:9, name:"Paul Lim", course:"BSIT" },
+    { id:10, name:"Sarah Yu", course:"BSCS" }
+  ];
+
+  useEffect(() => {
+    setStudents(studentData);
+  }, []);
+
+  return (
+    <div>
+      <h2>Student List</h2>
+    </div>
+  );
+}
+
+export default Students;
