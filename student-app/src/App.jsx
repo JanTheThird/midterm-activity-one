f48d6876adf26729e05e6c1dad9fed02d6f5efd8
JanTheThird <20122613@s.ubaguio.edu>
2026-03-09 21:05:06 +0800

    Configured the react router

--- student-app/src/App.jsx
+++ student-app/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App
