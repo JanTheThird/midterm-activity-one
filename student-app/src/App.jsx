7f86dbb5bccdba06b4f0a6a5ac2e3a1c065dcf37
JanTheThird <20122613@s.ubaguio.edu>
2026-03-09 21:44:49 +0800

    Created navigation component and setup the router

--- student-app/src/App.jsx
+++ student-app/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+import HomePage from './Pages/HomePage'; 
+import Students from './Pages/Students'; 
+import About from './Pages/About';
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar /> 
+      
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/students" element={<Students />} />
+        <Route path="/about" element={<About />} /> 
       </Routes>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ student-app/src/Components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+      <Link className="navbar-brand" to="/">My Project</Link>
+      
+      <div className="navbar-nav">
+        <Link className="nav-link" to="/HomePage">Home</Link>
+        <Link className="nav-link" to="/students">Students</Link>
+        <Link className="nav-link" to="/about">About</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
